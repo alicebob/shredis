ad44c744aaf5ad1ef43a8826cee31b0eb6399796
Harmen <harm@dds.nl>
2015-02-28 13:10:44 +0100

    more exampling

--- example_test.go
+++ example_test.go
@@ -13,17 +13,44 @@ func main() {
 		"shard2": "127.0.0.1:6391",
 		"shard3": "127.0.0.1:6392",
 	})
+	defer shr.Close()
+
+	// Build the commands.
 	cs := []*shredis.Cmd{
 		shredis.Build("Key", "GET", "Key"),
 		shredis.Build("Key2", "GET", "Key2"),
 	}
-	shr.Exec(cs[0], cs[1])
+	// Send to all redises. This blocks.
+	shr.Exec(cs...)
+	// Check the results.
 	for _, c := range cs {
 		res, err := c.Get()
 		if err != nil {
 			fmt.Printf("Command err: %s\n", err)
 			continue
 		}
-		fmt.Printf("Result: %s\n", res)
+		fmt.Printf("Result: %v\n", res)
 	}
+
+	// Or keep the commands around:
+	set := shredis.BuildSet("Key2", "Value")
+	get := shredis.BuildGet("Key2")
+	shr.Exec(set, get)
+	if _, err := set.Get(); err != nil {
+		fmt.Printf("set error: %s", err)
+	}
+
+	v, err := get.Get()
+	if err != nil {
+		fmt.Printf("get error: %s", err)
+	}
+	fmt.Printf("got: %q", string(v.([]byte)))
+
+	// Or use a Get* helper to deal with the interface{}:
+	var vs string
+	if vs, err = get.GetString(); err != nil {
+		fmt.Printf("get error: %s", err)
+	}
+	fmt.Printf("got: %q", vs)
+
 }
